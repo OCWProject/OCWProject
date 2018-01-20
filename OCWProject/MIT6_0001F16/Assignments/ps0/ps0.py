566372e9de5837dd778e7173f22832b052dff8d7
OCWProject <OCWProjector@gmail.com>
2018-01-20 15:38:07 +0700

    MITOCW

--- OCWProject/MIT6_0001F16/Assignments/ps0/ps0.py
+++ OCWProject/MIT6_0001F16/Assignments/ps0/ps0.py
@@ -1,12 +1,14 @@
-# MIT6_0001: PROBLEM SET 1
+# # MIT6_0001: PROBLEM SET 1
+#
+#
+# import math
+#
+# # Input integers x and y
+# # Calculates x power of y and logarithms based 2 of x
+# # Done! Have been tested
+# x = int(input('Enter x = '))
+# y = int(input('Enter y = '))
+# print('x**y = ', x**y)
+# print('log(x) = ', math.log(x, 2))
+#
 
-
-import math
-
-# Input integers x and y
-# Calculates x power of y and logarithms based 2 of x
-# Done! Have been tested
-x = int(input('Enter x = '))
-y = int(input('Enter y = '))
-print('x**y = ', x**y)
-print('log(x) = ', math.log(x, 2))
